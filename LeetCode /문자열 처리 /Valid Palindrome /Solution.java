d1c1398b014b3c9bfe054967ba80f2bf8a2ec1dc
Joon <83629193+Joon9750@users.noreply.github.com>
2024-11-03 20:49:52 +0900

    Delete LeetCode  directory

--- LeetCode /문자열 처리 /Valid Palindrome /Solution.java
+++
@@ -1,8 +0,0 @@
-class Solution {
-    public boolean isPalindrome(String s) {
-
-        String s_filtered = s.replaceAll("[^A-Za-z0-9]", "").toLowerCase();
-        String s_reversed = new StringBuilder(s_filtered).reverse().toString();
-        return s_filtered.equals(s_reversed);
-    }
-}
